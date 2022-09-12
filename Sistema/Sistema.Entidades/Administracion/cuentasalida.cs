7fae2d796cb68f49a3262b07e6fa3ee079da8a3c
Dantte Quiroz <dantte16@gmail.com>
2022-09-12 14:18:42 -0400

    entidades carga banco y cuenta salida

---
+++ Sistema/Sistema.Entidades/Administracion/cuentasalida.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Entidades.Administracion
+{
+    public class cuentasalida
+    {
+        public int idcuentasalida { get; set; }
+
+        [Required]
+        [ForeignKey("banco")]
+        public int idbanco { get; set; }
+        [Required]
+        [ForeignKey("moneda")]
+        public int idmoneda { get; set; }
+        public string cuenta { get; set; }
+        public string descripcion { get; set; }
+        public bool condicion { get; set; }
+
+
+        public Banco banco { get; set; }
+        public Moneda moneda { get; set; }
+
+
+
+        //public ICollection<Ordendepago> ordendepago { get; set; }
+    }
+}
---
+++ Sistema/Sistema.Entidades/Tesoreria/op_cargado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Entidades.Tesoreria
+{
+    public class op_cargado
+    {
+        public int idcargado { get; set; }
+        [Required]
+        [ForeignKey("ordendepago")]
+        public int idordendepago { get; set; }
+        [Required]
+        [ForeignKey("cuentasalida")]
+        public int idcuentasalid { get; set; }
+        public string lote { get; set; }
+        public DateTime fecha { get; set; }
+
+        //public ICollection<Ordendepago> ordendepago { get; set; }
+    }
+}
