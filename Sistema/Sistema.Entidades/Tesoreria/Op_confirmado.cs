288e3dc37f76db0a872139efdc5e6b90dc16ecc0
Dantte Quiroz <dantte16@gmail.com>
2022-09-13 14:01:05 -0400

    entidad op_confirmado

---
+++ Sistema/Sistema.Entidades/Tesoreria/Op_confirmado.cs
@@ -0,0 +1,22 @@
+﻿using Sistema.Entidades.Solicitud;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Entidades.Tesoreria
+{
+    public class Op_confirmado
+    {
+        public int idconfirmado { get; set; }
+        [Required]
+        [ForeignKey("ordendepago")]
+        public int idordendepago { get; set; }
+        public DateTime fecha { get; set; }
+
+        public Ordendepago ordendepago { get; set; }
+    }
+}
