7876148ba8fbcaaab8c6a0bc3219a5c09de6ff11
Dantte Quiroz <dantte16@gmail.com>
2022-09-12 20:20:44 -0400

    ViewModel Op_cargado

---
+++ Sistema/Sistema.Web/Models/Tesoreria/Op_cargado/Op_cargadoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Web.Models.Tesoreria.Op_cargado
+{
+    public class Op_cargadoViewModel
+    {
+        public string idcuentasalida { get; set; }
+        public string cuentasalida { get; set; }
+        public string nombrecuenta { get; set; }
+        public string lote { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
