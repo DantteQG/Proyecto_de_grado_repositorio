d32837a277645cffc36add92274138b8c67f022c
Dantte Quiroz <dantte16@gmail.com>
2022-09-13 14:14:26 -0400

    Viewmodels y controlador Op_confirmados

---
+++ Sistema/Sistema.Web/Controllers/Op_confirmadosController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sistema.Datos;
+using Sistema.Entidades.Tesoreria;
+
+namespace Sistema.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Op_confirmadosController : ControllerBase
+    {
+        private readonly DbContextSistema _context;
+
+        public Op_confirmadosController(DbContextSistema context)
+        {
+            _context = context;
+        }
+
+        
+
+        private bool Op_confirmadoExists(int id)
+        {
+            return _context.op_Confirmados.Any(e => e.idconfirmado == id);
+        }
+    }
+}
---
+++ Sistema/Sistema.Web/Models/Tesoreria/Op_confirmado/CrearViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Web.Models.Tesoreria.Op_confirmado
+{
+    public class CrearViewModel
+    {
+        public int idordendepago { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
