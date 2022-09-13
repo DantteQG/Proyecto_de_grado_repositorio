24a58a5f3efcb47549c3b0d5f243e21787b23056
Dantte Quiroz <dantte16@gmail.com>
2022-09-13 14:04:59 -0400

    mapeamiento de op_confirmado

---
+++ Sistema/Sistema.Datos/Mapping/Tesoreria/Op_confirmadoMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sistema.Entidades.Tesoreria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Datos.Mapping.Tesoreria
+{
+    class Op_confirmadoMap : IEntityTypeConfiguration<Op_confirmado>
+    {
+        public void Configure(EntityTypeBuilder<Op_confirmado> builder)
+        {
+            builder.ToTable("op_confirmado")
+                .HasKey(oc => oc.idconfirmado);
+
+        }
+
+    }
+}
