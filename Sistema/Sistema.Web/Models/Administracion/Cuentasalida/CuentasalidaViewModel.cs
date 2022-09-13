2701822262958759db54f2601cc1b48b08da569b
Dantte Quiroz <dantte16@gmail.com>
2022-09-12 20:15:51 -0400

    ViewModel  y select cuentasalida

---
+++ Sistema/Sistema.Web/Models/Administracion/Cuentasalida/CuentasalidaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Web.Models.Administracion.Cuentasalida
+{
+    public class CuentasalidaViewModel
+    {
+        public int idcuentasalida { get; set; }
+        public int idbanco { get; set; }
+        public string banco { get; set; }
+        public int idmoneda { get; set; }
+        public string moneda { get; set; }
+        public string cuenta { get; set; }
+        public string descripcion { get; set; }
+        public bool condicion { get; set; }
+
+    }
+}
---
+++ Sistema/Sistema.Web/Models/Administracion/Cuentasalida/SelectViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Web.Models.Administracion.Cuentasalida
+{
+    public class SelectViewModel
+    {
+        public int idcuentasalida { get; set; }
+        public string banco { get; set; }
+        public string moneda { get; set; }
+        public string cuenta { get; set; }
+
+    }
+}
